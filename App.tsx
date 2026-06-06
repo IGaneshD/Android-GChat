626990cf0675296eaf209abf9f502a8953dc4e43
Ganesha <rajdhani.blog@gmail.com>
2026-06-06 12:26:30 +0530

    feat:initial setup

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, Button, TextInput } from 'react-native';
 import {
   SafeAreaProvider,
+  SafeAreaView,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
@@ -17,8 +18,8 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}  />
+      <AppContent/>
     </SafeAreaProvider>
   );
 }
@@ -27,12 +28,13 @@ function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titleText}>
+        Hi, This is Ganesh
+      </Text>
+      <Button title="hello"></Button>
+      {/* <TextInput placeholder='Hello Enter Your Name'></TextInput> */}
+    </SafeAreaView>
   );
 }
 
@@ -40,6 +42,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  titleText :{
+    color: '#ffffff',
+    backgroundColor:'red'
+  }
 });
 
 export default App;
